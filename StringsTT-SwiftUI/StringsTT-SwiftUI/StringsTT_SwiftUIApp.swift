6f87f7b4e8e7f27e5e2facc6651631d38c22f37a
EvenLin <evenlinyf@gmail.com>
2022-04-13 22:46:44 +0800

    SwiftUI version

---
+++ StringsTT-SwiftUI/StringsTT-SwiftUI/StringsTT_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringsTT_SwiftUIApp.swift
+//  StringsTT-SwiftUI
+//
+//  Created by EvenLin on 2022/4/13.
+//
+
+import SwiftUI
+
+@main
+struct StringsTT_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
